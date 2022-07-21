350dfbaa81e97111443667b620e229962eea1377
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-07-21 15:01:45 -0700

    Build class-based Loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "components/Loading";
 
 class Dashboard extends Component {
+
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
